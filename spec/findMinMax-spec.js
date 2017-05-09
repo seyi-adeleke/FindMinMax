5ec045b78650086239b0225ebcc8b62efffdf929
Seyi Adeleke <seyiadeleke42@yahoo.com>
2017-05-09 12:04:43 +0100

    passing all test cases

--- spec/findMinMax-spec.js
+++ spec/findMinMax-spec.js
@@ -8,15 +8,15 @@ const find = require("../findMinMax");
         describe('Return the min and max number in the list in a new list follows `[min, max]`', function () {
 
             it('should return [1,4] for [1, 2, 3 , 4]', function () {
-                expect(findMinMax([1, 2, 3, 4])).toEqual([1, 4]);
+                expect(find.findMinMax([1, 2, 3, 4])).toEqual([1, 4]);
             });
 
             it('should return [4, 6] for [6, 4]', function () {
-                expect(findMinMax([6, 4])).toEqual([4, 6]);
+                expect(find.findMinMax([6, 4])).toEqual([4, 6]);
             });
 
             it('should return [2, 78] for [4, 66, 6, 44, 7, 78, 8, 68, 2]', function () {
-                expect(findMinMax([4, 66, 6, 44, 7, 78, 8, 68, 2])).toEqual([2, 78]);
+                expect(find.findMinMax([4, 66, 6, 44, 7, 78, 8, 68, 2])).toEqual([2, 78]);
             });
 
         });
@@ -24,7 +24,7 @@ const find = require("../findMinMax");
         describe('Return the number in the list in a new list follows `[min]` when the number is the min and max number in that list', function () {
 
             it('should return [4] for [4, 4, 4, 4]', function () {
-                expect(findMinMax([4, 4, 4, 4])).toEqual([4]);
+                expect(find.findMinMax([4, 4, 4, 4])).toEqual([4]);
             });
 
         });
